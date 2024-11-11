08f3aff0758b933e185a738e8c93f34200ad7ad9
Florenz Ramos <faramos@pup.edu.ph>
2024-11-11 16:38:58 +0800

    fix navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Headers from './components/landing-page/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Pricing from './components/pricing-page/Pricing';
+import MainPage from './components/landing-page/MainPage';
 
 function App() {
   return (
-    <Headers/>
+  <>
+
+     <BrowserRouter basename='/hris'>
+      <Headers/>
+        <Routes>
+          <Route path="/" index element={<MainPage />} />
+          <Route path="pricing" element={<Pricing />} />
+        </Routes>
+      </BrowserRouter>
+  </>
   );
 }
 
